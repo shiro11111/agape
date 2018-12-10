6ea799fb3270bcfd4d46d7e9ceea6abfccae49a6
Małgorzata Szlęzak <ameba01@poczta.fm>
2018-12-10 12:59:16 +0100

    - redux

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ChurchEvent } from '../../events-interface';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { LoadEventsList } from '../events.actions';
+import { EventsService } from '../events.service';
+import { map } from 'rxjs/operators';
+import { EventsState } from '../events.reducers';
 
 @Component({
   selector: 'app-wydarzenia-lista',
@@ -6,10 +13,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent implements OnInit {
+  list$: Observable<ChurchEvent[]>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.dispatch(new LoadEventsList());
+
+    this.list$ = this.store.select('eventsState').pipe(
+      map((state: EventsState) => state && state.list));
   }
 
 }
